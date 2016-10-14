c7bcaa532c81b210c9b4f1b98e9f3574a8e7bd80
CorvetteCole <corvettecole@gmail.com>
2016-10-14 08:53:38 -0400

    Allows up to 4 sensors to be measured
    Should be able to measure numbers from 4 different sensors and distinguish between them in the serial monitor. Also easily scaled up to more sensors

--- IrSensorReading.ino
+++ IrSensorReading.ino
@@ -1,7 +1,7 @@
-/*code should provide numbers from two sensors simultaneously. Still need to distinguish which is which though.
+/*code should provide numbers from 4 sensors simultaneously.
  * 
  */
-int sensor1,sensor2 = 0;
+int sensor1,sensor2,sensor3,sensor4 = 0;
 
 void setup() {
   Serial.begin(9600);              // Use Serial Monitor window
@@ -10,9 +10,15 @@ void setup() {
 void loop() {
   sensor1 = analogRead(A0);  // Read the sensor
   sensor2 = analogRead(A1);
+  sensor3 = analogRead(A2);
+  sensor4 = analogRead(A3);
   Serial.println("Sensor1");   
   Serial.println(sensor1, DEC);   // Display value in Serial Monitor window
   Serial.println("Sensor2");
   Serial.println(sensor2, DEC);
+  Serial.println("Sensor3");
+  Serial.println(sensor3, DEC);
+  Serial.println("Sensor4");
+  Serial.println(sensor4, DEC);
   delay(250);                      // Wait 1/4 second for the next read
 }
