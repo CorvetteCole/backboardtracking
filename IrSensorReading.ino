d90353cb46858bf23745205e414aaf1b283b79dc
RadioactiveFlowers <iamame1999@gmail.com>
2016-10-14 12:16:14 -0400

    fix parenthesis

--- IrSensorReading.ino
+++ IrSensorReading.ino
@@ -23,20 +23,20 @@ void loop() {
   delay(250);                      // Wait 1/4 second for the next read
 }
 void loop() {
-	if 0 < sensor1 < 255
+	if (0 < sensor1 < 255)
 		sensor1dist = 1;            // This likely isn't the fastest or most efficient way of doing it (I don't even know if it works).
-	if 255 < sensor1 < 510
+	if (255 < sensor1 < 510)
 		sensor1dist = 2;
-	if 510 < sensor1 < 765
+	if (510 < sensor1 < 765)
 		sensor1dist = 3;
-	if 765 < sensor1 < 1020      // Values in final version will be very different to reduce false readings
+	if (765 < sensor1 < 1020)      // Values in final version will be very different to reduce false readings
 		sensor1dist = 4;
-	if 0 < sensor2 < 255           //sensor 2 referencing begins
+	if (0 < sensor2 < 255)           //sensor 2 referencing begins
 		sensor2dist = 1;
-	if 255 < sensor2 < 510
+	if (255 < sensor2 < 510)
 		sensor2dist = 2;
-	if 510 < sensor2 < 765
+	if (510 < sensor2 < 765)
 		sensor2dist = 3;
-	if 765 < sensor2 < 1020
+	if (765 < sensor2 < 1020)
 		sensor2dist = 4;
 }
