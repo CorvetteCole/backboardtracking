5c19891806d7e1edd6a20a6a3d1359b1fb77b312
CorvetteCole <corvettecole@gmail.com>
2016-10-14 08:27:36 -0400

    Initial files. Should allow sensor values of 2 sensors to be read
    Should be easily scale able. To do: add 1 and 2 tags to sensor outputs to distinguish them.

---
+++ IrSensorReading.ino
@@ -0,0 +1,16 @@
+/*code should provide numbers from two sensors simultaneously. Still need to distinguish which is which though.
+ * 
+ */
+int sensor1,sensor2 = 0;
+
+void setup() {
+  Serial.begin(9600);              // Use Serial Monitor window
+}
+
+void loop() {
+  sensor1 = analogRead(A0);  // Read the sensor
+  sensor2 = analogRead(A1);
+  Serial.println(sensor1, DEC);   // Display value in Serial Monitor window
+  Serial.println(sensor2, DEC);
+  delay(250);                      // Wait 1/4 second for the next read
+}
