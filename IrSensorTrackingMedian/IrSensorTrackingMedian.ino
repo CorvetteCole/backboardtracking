96270ea86d0d07d55190d4d1fb6c7aa616b5ca8c
Cole Gerdemann <Cole Gerdemann>
2016-11-10 12:21:13 -0500

    Alright so debugging code has been disabled (activation isntructions in readme) and code has been finalized for now (1 sensor) it vorks!

--- IrSensorTrackingMedian/IrSensorTrackingMedian.ino
+++ IrSensorTrackingMedian/IrSensorTrackingMedian.ino
@@ -6,8 +6,7 @@ int sensor1dist,sensor2dist,sensor3dist,sensor4dist = 0;
 float med1 [3] = { };
 float med2 [3] = { };
 float med3 [3] = { };
-void sort(float a[], float size) {
-  Serial.println("sorting...");
+void sort(float a[], float size) { //deb:Serial.println("sorting...");  
     for(int i=0; i<(size-1); i++) {
         for(int o=0; o<(size-(i+1)); o++) {
                 if(a[o] > a[o+1]) {
@@ -23,72 +22,50 @@ void setup() {
 }
 void volt(int sensor){
   if (sensor == 1){ 
-  sensor1voltage = analogRead(A0) * (5.0 / 1023.0); 
-  Serial.println("sensor1 read");}
-  else if (sensor == 2){
-  Serial.println("sensor2 read");
-  sensor2voltage = analogRead(A1) * (5.0 / 1023.0); }
-  else if (sensor == 3){
-  Serial.println("sensor3 read");
-  sensor3voltage = analogRead(A2) * (5.0 / 1023.0); }
-  else if (sensor == 4){
-  Serial.println("sensor4 read");
-  sensor4voltage = analogRead(A3) * (5.0 / 1023.0); }
+  sensor1voltage = analogRead(A0) * (5.0 / 1023.0); //deb:Serial.println("sensor1 read");
+  }
+  else if (sensor == 2){                            //deb:Serial.println("sensor2 read");
+  sensor2voltage = analogRead(A1) * (5.0 / 1023.0); 
+  }
+  else if (sensor == 3){                            //deb:Serial.println("sensor3 read");
+  sensor3voltage = analogRead(A2) * (5.0 / 1023.0); 
+  }
+  else if (sensor == 4){                            //deb:Serial.println("sensor4 read");
+  sensor4voltage = analogRead(A3) * (5.0 / 1023.0); 
+  }
 }
 void loop() {
-  volt(1);
-  Serial.println("Volt1 ran");
-  /*volt(2);
+  volt(1);                                          //deb:Serial.println("Volt1 ran");
+  volt(2);
   volt(3);
-  volt(4);*/
+  volt(4);
   Serial.println(sensor1voltage, DEC);
-    if (1.66 <= sensor1voltage && sensor1voltage <= 2.5) {
-      Serial.println("sensor1 if statement activated");
-      Serial.println(sensor1voltage, DEC);      
-      med1 [0] = sensor1voltage;
-      Serial.println("first array value assigned to sensor1voltage");  
-      delay(38);
-      Serial.println("1st 38 ms delay finished");
-      volt(1);
-      Serial.println("volt1 for array value 2 ran"); 
-      Serial.println(sensor1voltage, DEC);
-      med1 [1] = sensor1voltage;   
-      Serial.println("Value 2 of array assigned");
-      delay(38);
-      Serial.println("2nd 38 ms delay finished");
-      volt(1);
-      Serial.println("volt1 for array value 3 ran");
-      Serial.println(sensor1voltage, DEC);
-      med1 [2] = sensor1voltage;   
-      Serial.println("Value 3 of array assigned");
-      sort(med1,3);
-      Serial.println("Array sort ran");
-      Serial.println(med1 [1], DEC);
-      sensor1voltage = med1 [1]; 
-      Serial.println("sensor1voltage assigned from value 2 in array");
-      Serial.println(sensor1voltage, DEC);
+    if (1.66 <= sensor1voltage && sensor1voltage <= 2.5) {  //deb:Serial.println("sensor1 if statement activated");  //deb:Serial.println(sensor1voltage, DEC);      
+      med1 [0] = sensor1voltage;                    //deb:Serial.println("first array value assigned to sensor1voltage");  
+      delay(38);                                    //deb:Serial.println("1st 38 ms delay finished");
+      volt(1);                                      //deb:Serial.println("volt1 for array value 2 ran"); //deb:Serial.println(sensor1voltage, DEC);
+      med1 [1] = sensor1voltage;                    //deb:Serial.println("Value 2 of array assigned");
+      delay(38);                                    //deb:Serial.println("2nd 38 ms delay finished");
+      volt(1);                                      //deb:Serial.println("volt1 for array value 3 ran"); //deb:Serial.println(sensor1voltage, DEC);
+      med1 [2] = sensor1voltage;                    //deb:Serial.println("Value 3 of array assigned");
+      sort(med1,3);                                 //deb:Serial.println("Array sort ran"); //deb:Serial.println(med1 [1], DEC);
+      sensor1voltage = med1 [1];                    //deb:Serial.println("sensor1voltage assigned from value 2 in array"); //deb:Serial.println(sensor1voltage, DEC);
                       
-       if (2.12 < sensor1voltage && sensor1voltage < 2.5) {
-      	Serial.println("block1 triggered");
-      	sensor1dist = 1; 
-      	Serial.println(sensor1dist, DEC);}                      
-       else if (1.93 < sensor1voltage && sensor1voltage < 2.12) {
-        Serial.println("block2 triggered");
-        sensor1dist = 2; 
-        Serial.println(sensor1dist, DEC);}   
-       else if (1.82 < sensor1voltage && sensor1voltage < 1.93) {
-      	Serial.println("block3 triggered");
-      	sensor1dist = 3; 
-      	Serial.println(sensor1dist, DEC);}   
-       else if (1.73 < sensor1voltage && sensor1voltage < 1.82) {       
-      	Serial.println("block4 triggered");
-      	sensor1dist = 4;
-      	Serial.println(sensor1dist, DEC);}   
-       else if (1.66 < sensor1voltage && sensor1voltage < 1.73) {
-        Serial.println("block5 triggered");
-        sensor1dist = 5; 
-        Serial.println(sensor1dist, DEC);}        
-       
+       if (2.12 < sensor1voltage && sensor1voltage < 2.5) {   //deb:Serial.println("block1 triggered");
+      	sensor1dist = 1;                                      //deb:Serial.println(sensor1dist, DEC);}                      
+       }
+       else if (1.93 < sensor1voltage && sensor1voltage < 2.12) {    //deb:Serial.println("block2 triggered");
+        sensor1dist = 2;                                             //deb:Serial.println(sensor1dist, DEC);     
+       }
+       else if (1.82 < sensor1voltage && sensor1voltage < 1.93) {    //deb:Serial.println("block3 triggered");
+      	sensor1dist = 3;                                             //deb:Serial.println(sensor1dist, DEC);   
+       }
+       else if (1.73 < sensor1voltage && sensor1voltage < 1.82) {    //deb:Serial.println("block4 triggered");
+      	sensor1dist = 4;                                             //deb:Serial.println(sensor1dist, DEC);   
+       }
+       else if (1.66 < sensor1voltage && sensor1voltage < 1.73) {    //deb:Serial.println("block5 triggered");
+        sensor1dist = 5;                                             //deb:Serial.println(sensor1dist, DEC);  
+       }
     }    
     /*if (1.66 < sensor2voltage && sensor2voltage < 2.5) {
      Serial.println("Broken2");
--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # IrSensorTracking
 Allows the finding of generalized location of where a basketball hit on the backboard of a basketball goal.
 
-
-
-#VoltageMeasuring
+# VoltageMeasuring
 Tool provided in folder to measure voltages from the analog pin a sensor is connected to. Tracking code is independant of this file. In other words, this isn't needed to run the main code.
 
-#To-do
+# To-do
 -Now that bugs are sorted out, fully implement code to support multiple sensors in median code.
 -Update other sensor tracking code to support the 'only run when in range' method. This way is now working on the median code and only runs the main bulk of the code when something falls within the sensor range. On the median code this also allows for a 2 step failsafe to eliminate false positives. The main median code only runs when something is within sensor range. From there, it takes two more readings and if those are outside of sensor range it will disregard it as an anomaly and just continue. Really quite simple and ingenius. 
 
 
-#Development tools used
+# Development tools used
 UnoArduSim V1.6.3: Used and allowed me to see exactly where my code was decided to run awry. Would highly reccommend. Its fairly intuitive as well.
 ArduinoIDE: Pretty self explanatory. Just the official Arduino environment
 Github: Used to manage this repository
+
+# Debugging
+To enable debugging (only on median code for now) hit control+F, find "//deb:", leave the replace field blank, and click replace all. Check serial monitor for debugging info
