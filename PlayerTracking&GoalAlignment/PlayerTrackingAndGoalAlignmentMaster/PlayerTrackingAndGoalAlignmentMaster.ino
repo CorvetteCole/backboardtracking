12f1cb3f96873b5de627858139ccdaafb905525b
Cole Gerdemann <corvettecole@gmail.com>
2017-04-14 14:39:08 -0400

    motor angle test updated to use new encoder method. New encoder method optimized further. New encoder methods and calculations and everything really is being integrated in to the MasterTracking code

--- PlayerTracking&GoalAlignment/PlayerTrackingAndGoalAlignmentMaster/PlayerTrackingAndGoalAlignmentMaster.ino
+++ PlayerTracking&GoalAlignment/PlayerTrackingAndGoalAlignmentMaster/PlayerTrackingAndGoalAlignmentMaster.ino
@@ -1,29 +1,51 @@
-int angle, encoderValue, u = 0;
-float radian, x, y;
-const float Pi = 3.14159;
-void alignment(void), count(void), forward(void), reverse(void);
-
+//Libraries
+#include <digitalWriteFast.h>   
 #include <RFM69.h>
 #include <RFM69registers.h>
 #include <RFM69_ATC.h> 
 
+//Radio setup
 #define NETWORKID     0   // Must be the same for all nodes
 #define MYNODEID      1   // My node ID
 #define TONODEID      2   // Destination node ID
-
 #define FREQUENCY     RF69_915MHZ
 #define ENCRYPT       false
 #define USEACK        false
 RFM69 radio;
 
+//Quadrature Encoder
+#define cEncoderInterruptA 0
+#define cEncoderInterruptB 1
+#define cEncoderPinA 2
+#define cEncoderPinB 3
+#define EncoderIsReversed
+volatile bool EncoderASet;
+volatile bool EncoderBSet;
+volatile bool EncoderAPrev;
+volatile bool EncoderBPrev;
+volatile long EncoderTicks = 0;
+
+//Other declarations
+int angle, encoderValue, PWM, ux, uy, u = 0;
+float x, y;
+void align(void);
+
+
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  //Initialize serial terminal
+  
+  //Radio setup
   radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
   radio.setHighPower(); // Always use this for RFM69HCW
   
-  pinMode(2, INPUT);   //replace 2 with whatever pin encoder is attached to
-  attachInterrupt(digitalPinToInterrupt(2), count, FALLING);
-  encoderValue = 0;
+  // Quadrature encoder
+  pinMode(cEncoderPinA, INPUT);      // sets pin A as input
+  digitalWrite(cEncoderPinA, LOW);  // turn on pullup resistors
+  pinMode(cEncoderPinB, INPUT);      // sets pin B as input
+  digitalWrite(cEncoderPinB, LOW);  // turn on pullup resistors
+  attachInterrupt(cEncoderInterruptA, HandleMotorInterruptA, CHANGE);
+  attachInterrupt(cEncoderInterruptB, HandleMotorInterruptB, CHANGE);
 }
 
 void loop() {
@@ -39,68 +61,113 @@ void loop() {
       Serial.print(u);
       Serial.print("\n" );
     if (u > 10) {     //x slave will add 30 to its value before transmission, y slave will not
-      y = u-30;
+      ux = u-30;
       u = 0;
     }
     if (u < 10) {
-      x = u;
+      uy = u;
       u = 0; 
     }
  
-   if ((x >= 1) && (y >= 1)) {
+   if (uy >= 1) {
  
-    Serial.println(x);
-    Serial.println(y);
+    Serial.println(ux);
+    Serial.print(", ");
+    Serial.print(uy);
     delay(500);
-    alignmotor();
+    align();
     }
   }
 }
 
-void alignmotor() {
-  Serial.println("it works");
-if (x != 0) {
-  radian = atan(y/x);
-  angle = (radian * 180)/Pi;
-}
-else if (x == 0) {
-  angle = 0;
-}
-else if ((y == 0) && (x < 0)) {
-  angle = 270;
-}
-else if ((y == 0) && (x > 0)) {
-  angle = 90;
+void align() {
+  encoderValue = encoderCalc();
+  angle = angleCalc();   
+   while ((encoderValue) != angle) { 
+    encoderValue = encoderCalc();
+    if (((encoderValue < angle+10)) && (encoderValue > (angle-10))) {
+      PWM = 60;
+    }
+    else PWM = 120;
+    if ((encoderValue) < angle) {
+      digitalWrite(8, HIGH);
+      digitalWrite(9, LOW);
+      analogWrite(5, PWM);   //0-255 range for motor speed  
+    }
+    if (encoderValue > angle) {
+      digitalWrite(8, LOW);
+      digitalWrite(9, HIGH);
+      analogWrite(5, PWM); 
+    }
+    Serial.println(encoderValue);
+   
+   
+      }
+      digitalWrite(8, LOW);
+      digitalWrite(9, LOW);
+}  
+
+int encoderCalc() {
+  int i;
+  i = EncoderTicks * (360/1856.0);
+  return i;
 }
 
-if (angle < 0) {
-  angle = 360 + angle;
-}
-
-if (angle < encoderValue) {
-  forward();
+int angleCalc() {
+ switch(ux) {
+  case -3:     //change x to equal each sensors respective distance from the backboard
+   x = 0;
+  case -2:
+   x = 0;
+  case -1:
+   x = 0;
+  case 0:
+   x = 0;
+  case 1:
+   x = 0;
+  case 2:
+   x = 0;
+  case 3:
+   x = 0;
  }
-if (angle > encoderValue) {
-  reverse();
- }
+ switch(uy) {
 }
 
-void forward() {
-  while (encoderValue != angle) {
-    digitalWrite(1,1); //forward motor pin
-  }   
+// Interrupt service routines for the  motor's quadrature encoder
+void HandleMotorInterruptA(){
+  EncoderBSet = digitalReadFast(cEncoderPinB);
+  EncoderASet = digitalReadFast(cEncoderPinA);
+  
+  EncoderTicks+=ParseEncoder();
+  
+  EncoderAPrev = EncoderASet;
+  EncoderBPrev = EncoderBSet;
 }
 
-void reverse() {
-  while (encoderValue != angle) {
-    digitalWrite(2,1); //reverse motor pin
+// Interrupt service routines for the right motor's quadrature encoder
+void HandleMotorInterruptB(){
+  // Test transition;
+  EncoderBSet = digitalReadFast(cEncoderPinB);
+  EncoderASet = digitalReadFast(cEncoderPinA);
+  
+  EncoderTicks+=ParseEncoder();
+  
+  EncoderAPrev = EncoderASet;
+  EncoderBPrev = EncoderBSet;
+}
+
+int ParseEncoder(){
+  if(EncoderAPrev && EncoderBPrev){
+    if(!EncoderASet && EncoderBSet) return 1;
+    if(EncoderASet && !EncoderBSet) return -1;
+  }else if(!EncoderAPrev && EncoderBPrev){
+    if(!EncoderASet && !EncoderBSet) return 1;
+    if(EncoderASet && EncoderBSet) return -1;
+  }else if(!EncoderAPrev && !EncoderBPrev){
+    if(EncoderASet && !EncoderBSet) return 1;
+    if(!EncoderASet && EncoderBSet) return -1;
+  }else if(EncoderAPrev && !EncoderBPrev){
+    if(EncoderASet && EncoderBSet) return 1;
+    if(!EncoderASet && !EncoderBSet) return -1;
   }
 }
-
-void count() {
-  encoderValue++;
-  Serial.print("Encoder value="); 
-  Serial.println(encoderValue);
-}
-
-
--- PlayerTracking&GoalAlignment/motorangletest/motorangletest.ino
+++ PlayerTracking&GoalAlignment/motorangletest/motorangletest.ino
@@ -2,54 +2,50 @@
 
 
 int angle, input, PWM;
-void count(void), align(void);
-
-#include <digitalWriteFast.h>
+void align(void);
 
 // It turns out that the regular digitalRead() calls are too slow and bring the arduino down when
 // I use them in the interrupt routines while the motor runs at full speed.
 
-// Quadrature encoders
-// Left encoder
-#define c_LeftEncoderInterruptA 0
-#define c_LeftEncoderInterruptB 1
-#define c_LeftEncoderPinA 2
-#define c_LeftEncoderPinB 3
-#define LeftEncoderIsReversed
+// Quadrature encoder
+#define cEncoderInterruptA 0
+#define cEncoderInterruptB 1
+#define cEncoderPinA 2
+#define cEncoderPinB 3
+#define EncoderIsReversed
 
-volatile bool _LeftEncoderASet;
-volatile bool _LeftEncoderBSet;
-volatile bool _LeftEncoderAPrev;
-volatile bool _LeftEncoderBPrev;
-volatile long _LeftEncoderTicks = 0;
+volatile bool EncoderASet;
+volatile bool EncoderBSet;
+volatile bool EncoderAPrev;
+volatile bool EncoderBPrev;
+volatile long EncoderTicks = 0;
 int encoderValue;
 
 void setup()  {
   Serial.begin(9600);
-// Quadrature encoders
-  // Left encoder
-  pinMode(c_LeftEncoderPinA, INPUT);      // sets pin A as input
-  digitalWrite(c_LeftEncoderPinA, LOW);  // turn on pullup resistors
-  pinMode(c_LeftEncoderPinB, INPUT);      // sets pin B as input
-  digitalWrite(c_LeftEncoderPinB, LOW);  // turn on pullup resistors
-  attachInterrupt(c_LeftEncoderInterruptA, HandleLeftMotorInterruptA, CHANGE);
-  attachInterrupt(c_LeftEncoderInterruptB, HandleLeftMotorInterruptB, CHANGE);
+// Quadrature encoder
+  pinMode(cEncoderPinA, INPUT);      // sets pin A as input
+  digitalWrite(cEncoderPinA, LOW);  // turn on pullup resistors
+  pinMode(cEncoderPinB, INPUT);      // sets pin B as input
+  digitalWrite(cEncoderPinB, LOW);  // turn on pullup resistors
+  attachInterrupt(cEncoderInterruptA, HandleMotorInterruptA, CHANGE);
+  attachInterrupt(cEncoderInterruptB, HandleMotorInterruptB, CHANGE);
   Serial.println("desired angle in degrees");
-  PWM = 255;
 } 
 
 void loop()  {
-    int encoderValue = encoderCalc();
     while (Serial.available() == 0) {}
     input = Serial.parseFloat();
     if (input != 0) {
       angle = input;
       align();
+      encoderValue = encoderCalc();
       Serial.println(encoderValue);
     }
 }
 
 void align(){  
+    encoderValue = encoderCalc();   
    while ((encoderValue) != angle) { 
     encoderValue = encoderCalc();
     if (((encoderValue < angle+10)) && (encoderValue > (angle-10))) {
@@ -75,48 +71,48 @@ void align(){
 }  
 
 
-// Interrupt service routines for the left motor's quadrature encoder
-void HandleLeftMotorInterruptA(){
-  _LeftEncoderBSet = digitalReadFast(c_LeftEncoderPinB);
-  _LeftEncoderASet = digitalReadFast(c_LeftEncoderPinA);
+// Interrupt service routines for the  motor's quadrature encoder
+void HandleMotorInterruptA(){
+  EncoderBSet = digitalReadFast(cEncoderPinB);
+  EncoderASet = digitalReadFast(cEncoderPinA);
   
-  _LeftEncoderTicks+=ParseEncoder();
+  EncoderTicks+=ParseEncoder();
   
-  _LeftEncoderAPrev = _LeftEncoderASet;
-  _LeftEncoderBPrev = _LeftEncoderBSet;
+  EncoderAPrev = EncoderASet;
+  EncoderBPrev = EncoderBSet;
 }
 
 // Interrupt service routines for the right motor's quadrature encoder
-void HandleLeftMotorInterruptB(){
+void HandleMotorInterruptB(){
   // Test transition;
-  _LeftEncoderBSet = digitalReadFast(c_LeftEncoderPinB);
-  _LeftEncoderASet = digitalReadFast(c_LeftEncoderPinA);
+  EncoderBSet = digitalReadFast(cEncoderPinB);
+  EncoderASet = digitalReadFast(cEncoderPinA);
   
-  _LeftEncoderTicks+=ParseEncoder();
+  EncoderTicks+=ParseEncoder();
   
-  _LeftEncoderAPrev = _LeftEncoderASet;
-  _LeftEncoderBPrev = _LeftEncoderBSet;
+  EncoderAPrev = EncoderASet;
+  EncoderBPrev = EncoderBSet;
 }
 
 int ParseEncoder(){
-  if(_LeftEncoderAPrev && _LeftEncoderBPrev){
-    if(!_LeftEncoderASet && _LeftEncoderBSet) return 1;
-    if(_LeftEncoderASet && !_LeftEncoderBSet) return -1;
-  }else if(!_LeftEncoderAPrev && _LeftEncoderBPrev){
-    if(!_LeftEncoderASet && !_LeftEncoderBSet) return 1;
-    if(_LeftEncoderASet && _LeftEncoderBSet) return -1;
-  }else if(!_LeftEncoderAPrev && !_LeftEncoderBPrev){
-    if(_LeftEncoderASet && !_LeftEncoderBSet) return 1;
-    if(!_LeftEncoderASet && _LeftEncoderBSet) return -1;
-  }else if(_LeftEncoderAPrev && !_LeftEncoderBPrev){
-    if(_LeftEncoderASet && _LeftEncoderBSet) return 1;
-    if(!_LeftEncoderASet && !_LeftEncoderBSet) return -1;
+  if(EncoderAPrev && EncoderBPrev){
+    if(!EncoderASet && EncoderBSet) return 1;
+    if(EncoderASet && !EncoderBSet) return -1;
+  }else if(!EncoderAPrev && EncoderBPrev){
+    if(!EncoderASet && !EncoderBSet) return 1;
+    if(EncoderASet && EncoderBSet) return -1;
+  }else if(!EncoderAPrev && !EncoderBPrev){
+    if(EncoderASet && !EncoderBSet) return 1;
+    if(!EncoderASet && EncoderBSet) return -1;
+  }else if(EncoderAPrev && !EncoderBPrev){
+    if(EncoderASet && EncoderBSet) return 1;
+    if(!EncoderASet && !EncoderBSet) return -1;
   }
 }
 
 int encoderCalc() {
   int i;
-  i = _LeftEncoderTicks * (360/1856.0);
+  i = EncoderTicks * (360/1856.0);
   return i;
 }
 
